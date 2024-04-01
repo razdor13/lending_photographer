ee6948014cf7c5fd64d84e49e988fae5f32ae4b1
Razdor13 <oneamaday@gmail.com>
2024-04-01 18:39:28 +0300

    Updates

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'amaday1488@gmail.com';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Password = 'kikist1996';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
